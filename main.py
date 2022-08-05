9778f8a746b7662f99cdd0b669ac2eae3e90b2dd
CameliaMallus <cameliamallus@gmail.com>
2022-08-05 12:27:30 +0200

    Translated French strings

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ if __name__=="__main__":
     a, b = GeneratePrimesUpTo(2_500_000)
     c, d = IsPrime(435000000000000011)     #Number of secs since the Big Bang   ≃40s to run
     #print_with_changed_separator(a, "\n")
-    print(f"En {b} secondes")
+    print(f"In {b} secondes")
     print(c) 
-    print(f"En {d} secondes")
+    print(f"In {d} secondes")
